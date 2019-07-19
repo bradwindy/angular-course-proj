4c3e7bfdc1d99f5c3ac3f8cf7c98bde5dce0df0f
Bradley Windybank <trykryda@gmail.com>
2019-07-20 00:11:59 +1200

    Update site content.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hi',
-  template: '<p>A CI template for an Angular app</p>'
+  template: '<p>A CI template for an Angular app. Hosted on GitHub pages.</p>'
 })
 export class CoursesComponent {}
