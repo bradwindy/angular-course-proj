12e2497a37f04dd03e63bc87964d346ce87c28c7
Bradley Windybank <trykryda@gmail.com>
2019-07-19 21:53:13 +1200

    Fixed component declaration error.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponentModule } from './courses.module';
-import { CoursesComponent } from './courses.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, CoursesComponentModule],
   providers: [],
   bootstrap: [AppComponent]
